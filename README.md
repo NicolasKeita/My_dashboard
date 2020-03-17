9690d6a3d28b88cb79301fc3b28715ad94562162
Nicolas KEITA <nicolas.keita@epitech.eu>
2020-03-17 19:09:48 +0100

    add: readme.md

---
+++ README.md
@@ -0,0 +1,14 @@
+# My_Dashboard
+My_Dashboard is a ...
+
+Here are some images from the website. EVERYTHING you see is fully working.
+
+### Preview
+![Settings](documentation/images/preview_connection_page.PNG)
+
+## Usage
+
+docker build && docker up
+
+This README.md is not finished (at all)
+
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   if (req.session.is_connected) {
     res.render('dashboard', {
       user_mail: req.session.user_email_connected
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
   } else {
     res.render('index', {
       title: 'Nicolas Super Dashboard',
-      title_sub: 'One man Army production'
+      title_sub: 'Hard work beats talent every time'
     });
   }
 });
