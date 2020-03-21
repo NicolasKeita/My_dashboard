32a6348863e9f877d2301694228fb7b543db4015
Nicolas KEITA <nicolas.keita@epitech.eu>
2020-03-21 20:58:41 +0100

    add: user can change google map focus localisation

--- public/js/youtubeViewCountWidget.js
+++ public/js/youtubeViewCountWidget.js
@@ -2,16 +2,12 @@
 
 let map;
 function initMap() {
-    //const { google } = require('googleapis');
-    //const goo = new google.maps();
-    var inputVal = document.getElementById("inputLatitude").value;
-    console.log("ici");
-    alert(inputVal);
-    inputVal = parseFloat(inputVal);
-    console.log(inputVal);
+    let inputLatitude = document.getElementById("inputLatitude").value;
+    let inputLongitude = document.getElementById("inputLongitude").value;
+    inputLatitude = parseFloat(inputLatitude);
+    inputLongitude = parseFloat(inputLongitude);
     map = new google.maps.Map(document.getElementById('map'), {
-//        center: {lat: -34.397, lng: 150.644},
-        center: {lat: inputVal, lng: 150.644},
-        zoom: 8,
+        center: {lat: inputLatitude, lng: inputLongitude},
+        zoom: 7,
     });
 }
