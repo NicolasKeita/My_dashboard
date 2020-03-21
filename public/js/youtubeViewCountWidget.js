6c88fe8dcd211c71c2be916ee0da44c0cfdae151
Nicolas KEITA <nicolas.keita@epitech.eu>
2020-03-21 03:52:32 +0100

    add: Google Map with possibility to Latitude on the map

--- public/js/youtubeViewCountWidget.js
+++ public/js/youtubeViewCountWidget.js
@@ -1,3 +1,17 @@
-let textt = document.querySelector("textt");
+//const
 
-textt.textContent = "Salut";
+let map;
+function initMap() {
+    //const { google } = require('googleapis');
+    //const goo = new google.maps();
+    var inputVal = document.getElementById("inputLatitude").value;
+    console.log("ici");
+    alert(inputVal);
+    inputVal = parseFloat(inputVal);
+    console.log(inputVal);
+    map = new google.maps.Map(document.getElementById('map'), {
+//        center: {lat: -34.397, lng: 150.644},
+        center: {lat: inputVal, lng: 150.644},
+        zoom: 8,
+    });
+}
